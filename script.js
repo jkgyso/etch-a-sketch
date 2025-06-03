481a04bcc0e06c894d702fbd1c908100c892bdf2
jkgyso <jkgyso@gmail.com>
2025-06-03 21:50:46 +0800

    feat: add extra functionalities(reset, radomize squares, increase opacity)

--- script.js
+++ script.js
@@ -1,22 +1,8 @@
-console.log('Hello World!');
-
-/*
-    What do we know? 
-    - we can create a single function that will create cols and rows 
-    - we'll pass a parameter that includes the size of the grid  
-    - create a separate element for columnDiv and rowDiv 
-    - 
-    - we can use a for loop to iterate the divs 
-    - add a classList for the columns and rows 
-    - add a width for the column 
-    - add a height for the row 
-    - append it to the gridContainer 
-
-*/
-
 const gridContainer = document.querySelector('#grid-squares-container');
 const squaresButton = document.querySelector('button');
-const randomColor = document.querySelector('input[name=random-color');
+const randomColor = document.querySelector('input[name=random-color]');
+const opacityCheckbox = document.querySelector('input[name=opacity]')
+const reset = document.querySelector('#reset');
 
 
 function createColumn(size) {
@@ -25,7 +11,6 @@ function createColumn(size) {
         let columnDiv = document.createElement('div');
         columnDiv.classList.add('col');
         columnDiv.style.width = 600 / size + 'px';
-        // columnDiv.textContent = `Col ${i}`
         gridContainer.appendChild(columnDiv);
         
     }
@@ -42,7 +27,6 @@ function createRow(size) {
             rowDiv.classList.add('row');
             rowDiv.classList.add('grid-tile');
             rowDiv.style.height = 600 / size + 'px';
-            // rowDiv.textContent = ``;
             columns[j].appendChild(rowDiv);
         }
     }
@@ -65,19 +49,8 @@ function addHoverEffect() {
 
 addHoverEffect();
 
-// function randomizeColor() {
-//     let tiles = document.querySelectorAll('.grid-tile');
-    
-//     let r = () => {
-//         return Math.floor(Math.random() * 256);
-//     }
 
-//     tiles.forEach(tile => {
-//         tile.addEventListener('mouseenter', () => {
-//             tile.style.backgroundColor = `rgb('${r}, ${r}, ${r}')`;
-//         })
-//     });
-// }
+
 
 
 
@@ -102,17 +75,66 @@ function addSquares() {
     
 } 
 
+
+function randomizeColor() {
+    let tiles = document.querySelectorAll('.grid-tile');
+    let randomCheckbox =  randomColor;
+    
+
+    tiles.forEach(tile => {
+        tile.addEventListener('mouseenter', () => {
+
+            if(randomCheckbox.checked) {
+                 let hex = '#' + Math.floor(Math.random()*16777215).toString(16);
+
+            tile.style.backgroundColor = `${hex}`;
+            } else {
+                tile.style.backgroundColor = 'yellow';
+            }
+
+           
+        })
+
+    });
+
+}
+
+function increaseOpacity() {
+    let tiles = document.querySelectorAll('.grid-tile');
+    let opacity = opacityCheckbox;
+
+        tiles.forEach(tile => {
+            tile.addEventListener('mouseenter', () => {
+
+            if(opacity.checked) {
+                 let currentOpacity = parseInt(tile.dataset.opacity) || 0;
+
+                 currentOpacity = Math.min(currentOpacity + 10, 100);
+
+                 tile.dataset.opacity = currentOpacity;
+                 tile.style.opacity = currentOpacity / 100; 
+
+            } else {
+                tile.style.opacity = '';
+                tile.dataset.opacity = 0;
+            }
+           
+        })
+
+    });
+
+}
+
+function resetColors() {
+    let tiles = document.querySelectorAll('.grid-tile');
+
+    tiles.forEach(tile => {
+        tile.style.backgroundColor = '';
+        tile.style.opacity = '';
+    })
+} 
+
 squaresButton.addEventListener('click', addSquares);
-// randomColor.addEventListener('change', randomizeColor);
-
-/*
-    Extra credit
-
-
-Rather than squares being the same color throughout the grid, randomize the squares’ RGB values with each interaction.
-
-Additionally, implement a progressive darkening effect where each interaction darkens the square by 10%. The goal is to achieve a fully black (or completely colored) square in only ten interactions.
-    Hint: The opacity CSS property is useful here. To learn how to use it, check this MDN docs article about the opacity CSS property.
-
-
-*/
+randomColor.addEventListener('change', randomizeColor);
+reset.addEventListener('click', resetColors);
+opacityCheckbox.addEventListener('change', increaseOpacity);
